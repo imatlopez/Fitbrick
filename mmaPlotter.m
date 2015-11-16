24f7b7b5d966b76813e4ff11bbfc25dc5de00906
Matias Lopez <mal70@duke.edu>
2015-11-15 22:41:02 -0500

    v0.0.6.1

--- mmaPlotter.m
+++ mmaPlotter.m
@@ -1,23 +1,26 @@
-fileID = fopen('test1.txt','r');
+fileID = fopen('test4.txt','r');
 
 dataArray = textscan(fileID,  '%f%f%f%[^\n\r]', 'Delimiter', ',', 'HeaderLines' ,1, 'ReturnOnError', false);
 fclose(fileID); X = dataArray{:, 1}; Y = dataArray{:, 2}; Z = dataArray{:, 3};
 clear fileID dataArray; close all
 
 T = 0.1:0.1:(0.1*length(X));
-A = sqrt(X.^2+Y.^2+Z.^2);
+A = sqrt(X.^2+Y.^2+Z.^2); A = (A-1).*(A>1);
 %figure(); plot(T,X,'r-',T,Y,'g-',T,Z,'b-');
-%figure(); plot(T, A,'k-');
+figure(); plot(T, A,'k-');
 
 % Averager
 ma = @(n) [2/n repmat(1/n, [1 n-1]) 2/n];
-A2 = conv(A,ma(5)); 
+A2 = conv(A,ma(3)); 
 %figure(); plot(A2,'k-');
 
 % Compound
-
 M = [mode(abs(X)), mode(abs(Y)), mode(abs(Z))];
 [~,i] = sort(M);
 K = [1 0.5 0.25]; K = K(i);
-Xi = abs(X)*K(1); Yi = abs(Y)*K(2); Zi = abs(Z)*K(3);
-figure(); plot(T,Xi+Yi+Zi);
+Xi = X*K(1); Yi = Y*K(2); Zi = Z*K(3);
+%figure(); plot(T,Xi+Yi+Zi);
+
+% Count Steps
+THRESH = 0.5;
+STEPS = 2*sum(diff(A>THRESH)>0)
